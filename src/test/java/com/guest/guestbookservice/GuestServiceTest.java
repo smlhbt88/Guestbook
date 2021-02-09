80d3b541788dacfb8686d8ba41c2ea1bbf9b5ef8
smlhbt88 <samuel.habte@cognizant.com>
2021-02-09 09:58:37 -0800

    Add visitor service test done

--- src/test/java/com/guest/guestbookservice/GuestServiceTest.java
+++ src/test/java/com/guest/guestbookservice/GuestServiceTest.java
@@ -1,14 +1,20 @@
 package com.guest.guestbookservice;
 
 
+import com.guest.guestbookservice.models.Visitor;
+import com.guest.guestbookservice.models.VisitorDto;
 import com.guest.guestbookservice.repositories.GuestRepository;
 import com.guest.guestbookservice.services.GuestService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
@@ -19,5 +25,19 @@ public class GuestServiceTest {
 
     @InjectMocks
     private GuestService guestService;
+
+    @Test
+    public void addVisitor() {
+        Visitor visitor = new Visitor("Eric","Very good");
+
+        when(guestRepository.save(visitor)).thenReturn(visitor);
+
+        VisitorDto actual = guestService.addVisitor(visitor);
+
+        verify(guestRepository, times(1)).save(visitor);
+
+        assertEquals(visitor.getName(), actual.getName());
+        assertEquals(visitor.getComment(), actual.getComment());
+    }
 
 }
